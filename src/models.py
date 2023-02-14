15a1a9f4b41ff4ec6fea044a01197dd1f3de92df
Sollb77 <qsiecuadorservicios@gmail.com>
2023-02-14 22:37:42 +0000

    final

--- src/models.py
+++ src/models.py
@@ -9,7 +9,7 @@ from eralchemy2 import render_er
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'User'
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id_User = Column(Integer, primary_key=True)
@@ -17,41 +17,40 @@ class User(Base):
     Password=Column(String(255), nullable=False)
 
 class Favorites(Base):
-    __tablename__ = 'Favorites'
+    __tablename__ = 'favorites'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id_Fa = Column(Integer, primary_key=True)
-    id_User = Column(String(250), nullable=False)
-    id_Pe= Column(String(250), nullable=False)
-    od_Pl = Column(String(250), nullable=False)
+    id_User = Column(Integer, ForeignKey("user.id_User"))
+    id_Pe= Column(Integer, ForeignKey("people.id_Pe"))
+    id_Pl = Column(Integer, ForeignKey("planet.id_Pl"))
     
 class People(Base):
-    __tablename__ = 'People'
+    __tablename__ = 'people'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id_Pe = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     gender = Column(String(250), nullable=False)
-    id_pl = Column(String(250), nullable=False)
-    id_Fi = Column(String(250), nullable=False)
+    id_Pl = Column(Integer, ForeignKey("planet.id_Pl"))
+    id_Fi = Column(Integer, ForeignKey("film.id_Fi"))
     
-    class Planet(Base):
-     __tablename__ = 'Planet'
+class Planet(Base):
+    __tablename__ = 'planet'
     id_Pl =Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     climate = Column(String(250), nullable=False)
     gravity = Column(String(250), nullable=False)
-    id_Fi = Column(String(250), nullable=False)
+   
 
-    class Film(Base):
-     __tablename__ = 'Film'
+class Film(Base):
+    __tablename__ = 'film'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id_Fi = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    id_Pe = Column(String(250), nullable=False)
     director = Column(String(250), nullable=False)
-    id_Pl = Column(String(250), nullable=False)
+    
 
     def to_dict(self):
         return {}
